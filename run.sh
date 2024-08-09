aea1c7d5ef3cca2b8506b2b2b6cb95a56ab2171f
Javier Gajardo <jgajardo@gmail.com>
2024-08-09 00:20:43 -0400

    sync

--- run.sh
+++ run.sh
@@ -13,7 +13,7 @@ sudo docker stop $host
 sleep 2
 sudo docker rm $host
 sleep 2
-sudo docker run -h ${host} --name $host -p 8080:8080 -e J_ADMIN_USER=newfms -e J_ADMIN_PASS=sdGT34934  -e J_MYSQL_DB=DESA  -e J_MYSQL_USER=desa -e J_MYSQL_PASSWORD=desa -e J_MYSQL_IP=10.68.3.120 -e J_MYSQL_PORT=3306  -itd ${tagname}
+sudo docker run -h ${host} --name $host -p 8080:8080 -e J_SYSLOG_USE=False -e J_SYSLOG_IP=127.0.0.1 -e J_SYSLOG_PORT=514 -e J_ADMIN_USER=newfms -e J_ADMIN_PASS=sdGT34934  -e J_MYSQL_DB=DESA  -e J_MYSQL_USER=desa -e J_MYSQL_PASSWORD=desa -e J_MYSQL_IP=10.68.3.120 -e J_MYSQL_PORT=3306  -itd ${tagname}
 sleep 2
 sudo docker logs ${host}
 
